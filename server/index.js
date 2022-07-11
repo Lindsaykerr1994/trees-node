a4f9737e610c98f5306d964266d33ec6a15900db
Lindsay Kerr <lindsaykerr1994@gmail.com>
2022-07-11 18:41:38 +0200

    Set up project to use express and mongoose

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
+require('dotenv').config();
 const express = require("express");
-
-const PORT = process.env.PORT || 8000;
-
 const app = express();
+const PORT = process.env.PORT || 8000;
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'));
+
+app.use(express.json())
+
+const treeRouter = require('../routes/trees');
+app.use('/trees')
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
